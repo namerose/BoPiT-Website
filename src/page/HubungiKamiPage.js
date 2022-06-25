9bb0f24346b92bc7620b89f873829f0a5aa0d4cb
razisek <agusdono27@gmail.com>
2022-06-25 22:30:08 +0700

    add gmaps in hubungi kami page

--- src/page/HubungiKamiPage.js
+++ src/page/HubungiKamiPage.js
@@ -34,15 +34,22 @@ function ContactUsPage() {
                             (0274) 884201
                         </div>
                     </div>
-                    <div className='text-2xl pt-12'>
-                        <div className='justify-center'>
-                            <LocationIcon className='inline mr-3' />
-                            <div className="text-2xl font-extrabold pt-12 inline">
-                                Alamat kami:
+                    <div className='grid grid-cols-2'>
+                        <div className='text-2xl pt-12'>
+                            <div className='justify-center'>
+                                <LocationIcon className='inline mr-3' />
+                                <div className="text-2xl font-extrabold pt-12 inline">
+                                    Alamat kami:
+                                </div>
+                            </div>
+                            <div className="mt-2">
+                                Jl. Ring Road Utara, Ngringin, Condongcatur, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281
                             </div>
                         </div>
-                        <div className="mt-2">
-                            Jl. Ring Road Utara, Ngringin, Condongcatur, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281
+                        <div className="-mt-40 ml-6">
+                            <div className="gmap_canvas">
+                                <iframe width="600" height="350" id="gmap_canvas" src="https://maps.google.com/maps?q=amikom%20yogyakarta&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                            </div>
                         </div>
                     </div>
                 </div>
