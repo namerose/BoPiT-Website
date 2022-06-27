349533db9dac34f43145502ff091337c43ea9def
StarSkyGeminid <akungithubku@gmail.com>
2022-06-27 16:20:21 +0700

    Add logout on sidebar and fix typo on page

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Logo from '../assets/image/LogoBerwarna.png'
 import { AiOutlineHome, AiOutlineSetting } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi";
 import { IoTvOutline } from "react-icons/io5";
 import { Link } from 'react-router-dom';
-
+import { logout } from '../firebase';
 function Sidebar({ children, page = 'beranda' }) {
+
+    const logOut = () => {
+        logout();
+    }
+
     return (
         <>
             <div className="flex">
                 <div className="flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto bg-dark-sidebar">
-                    <img src={Logo} alt='logo-bopit' className='w-24 self-center' />
-                    <div className="flex flex-col justify-between mt-6">
+                    <img src={Logo} alt='logo-bopit' className='w-28 self-center mt-6' />
+                    <div className="flex flex-col justify-between mt-14">
                         <aside>
                             <ul>
                                 <li>
@@ -34,9 +40,13 @@ function Sidebar({ children, page = 'beranda' }) {
                                     </Link>
                                 </li>
                             </ul>
-
                         </aside>
-
+                    </div>
+                    <div onClick={logOut} className="absolute bottom-5 w-fit ">
+                        <Link to='/' className={` bottom-5 w-48 cursor-pointer mt-2 flex items-center px-4 py-2 text-white-menu hover:bg-gray-200 rounded-md hover:text-dark-content }`}>
+                            <BiLogOut size={24} />
+                            <span className="mx-4 font-medium">Keluar</span>
+                        </Link>
                     </div>
                 </div>
                 <div className='w-full h-screen bg-dark-content'>
